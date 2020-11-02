0081ef1f4be05273c3a5c881af6de525ffca6c93
ElhamHamid <69770702+ElhamHamid@users.noreply.github.com>
2020-11-01 22:14:43 -0600

    updated
    Assignment 5 update

--- Assignment5/Tree.js
+++ Assignment5/Tree.js
@@ -1,3 +1,7 @@
+"use strict";
+/* eslint-disable*/
+
+
 class TPos {
     constructor(elem, parent, left, right) {
         this._parent = parent;
@@ -151,31 +155,3 @@ class Print extends EulerTour {
         console.log("[" + res + "]\n");
     }
 }
-
-
-
-var t0 = new BinaryTree();
-
-var printer = new Print();
-
-printer.print(t0);
-
-let r = t0.insertRoot(300);
-printer.print(t0);
-
-let l1 = t0.insertLeft(r, 200);
-let r1 = t0.insertRight(r, 400);
-printer.print(t0);
-t0.insertRight(l1, 250);
-l1 = t0.insertLeft(l1, 100);
-t0.insertRight(l1, 150);
-l1 = t0.insertLeft(l1, 50);
-l1 = t0.insertLeft(r1, 350);
-r1 = t0.insertRight(r1, 500);
-t0.insertLeft(r1, 450);
-r1 = t0.insertRight(r1, 600);
-t0.insertLeft(r1, 550);
-r1 = t0.insertRight(r1, 800);
-printer.print(t0);
-t0.insertLeft(r1, 700);
-printer.print(t0);
---
+++ Assignment5/tree_max.js
@@ -0,0 +1,47 @@
+"use strict";
+/* eslint-disable*/
+
+function Max(t){
+    return MaxHelper(t,t.root());
+}
+
+function MaxHelper(t,p){
+    let max=p;
+
+    if(t.isExternal(p)){
+        return -Infinity;
+    }
+    else{
+        let left=sumHelper(t,t.leftChild(p));
+        let right=sumHelper(t,t.rightChild(p));
+        return Math.max(max,left,right);
+    }
+}
+
+let test1 = new BinaryTree();
+
+let printer1 = new Print();
+
+printer1.print(test1);
+
+let s = test1.insertRoot(300);
+printer1.print(test1);
+
+let l2 = test1.insertLeft(s, 200);
+let r2 = test1.insertRight(s, 400);
+printer1.print(test1);
+test1.insertRight(l2, 250);
+l2 = test1.insertLeft(l2, 100);
+test1.insertRight(l2, 150);
+l2 = test1.insertLeft(l2, 50);
+l2 = test1.insertLeft(r2, 350);
+r2 = test1.insertRight(r2, 500);
+test1.insertLeft(r2, 450);
+r2 = test1.insertRight(r2, 600);
+test1.insertLeft(r2, 550);
+r2 = test1.insertRight(r2, 800);
+printer1.print(test1);
+test1.insertLeft(r2, 700);
+printer1.print(test1);
+
+console.log(Max(test1));
